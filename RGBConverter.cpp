57e7e6fb415554cb7fc00f0304ddb528ae02bc96
Scott Watson <scott@disney.com>
2011-02-22 00:56:08 -0800

    init

---
+++ RGBConverter.cpp
@@ -0,0 +1,157 @@
+#include "RGBConverter.h"
+/*
+ * original: https://github.com/ratkins/RGBConverter.git
+ *
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+void RGBConverter::rgbToHsl(byte r, byte g, byte b, float hsl[]) { 
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    byte max = threeway_max(r, g, b);
+    byte min = threeway_min(r, g, b);
+    byte h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        byte d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        if (max == r) {
+            h = (g - b) / d + (g < b ? 6 : 0);
+        } else if (max == g) {
+            h = (b - r) / d + 2;
+        } else if (max == b) {
+            h = (r - g) / d + 4;
+        }
+        h /= 6;
+    }
+    hsl[0] = h;
+    hsl[1] = s;
+    hsl[2] = l;
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+void RGBConverter::hslToRgb(float h, float s, float l, byte rgb[]) {
+    float r, g, b;
+
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        float q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        float p = 2 * l - q;
+        r = RGBConverter::hue2rgb(p, q, h + 1/3);
+        g = RGBConverter::hue2rgb(p, q, h);
+        b = RGBConverter::hue2rgb(p, q, h - 1/3);
+    }
+
+    rgb[0] = r * 255;
+    rgb[1] = g * 255;
+    rgb[2] = b * 255;
+}
+
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+void RGBConverter::rgbToHsv(byte r, byte g, byte b, float hsv[]) {
+    r = r/255, g = g/255, b = b/255;
+    byte max = threeway_max(r, g, b), min = threeway_min(r, g, b);
+    float h, s, v = max;
+
+    float d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) { 
+        h = 0; // achromatic
+    } else {
+        if (max == r) {
+            h = (g - b) / d + (g < b ? 6 : 0);
+        } else if (max == g) {
+            h = (b - r) / d + 2;
+        } else if (max == b) {
+            h = (r - g) / d + 4;
+        }
+        h /= 6;
+    }
+
+    hsv[0] = h;
+    hsv[1] = s;
+    hsv[2] = v;
+}
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+void RGBConverter::hsvToRgb(float h, float s, float v, byte rgb[]) {
+    float r, g, b;
+
+    int i = int(h * 6);
+    float f = h * 6 - i;
+    float p = v * (1 - s);
+    float q = v * (1 - f * s);
+    float t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+
+    rgb[0] = r * 255;
+    rgb[1] = g * 255;
+    rgb[2] = b * 255;
+}
+ 
+byte RGBConverter::threeway_max(byte a, byte b, byte c) {
+    return max(a, max(b, c));
+}
+
+byte RGBConverter::threeway_min(byte a, byte b, byte c) {
+    return min(a, min(b, c));
+}
+
+float RGBConverter::hue2rgb(float p, float q, float t) {
+    if(t < 0) t += 1;
+    if(t > 1) t -= 1;
+    if(t < 1/6) return p + (q - p) * 6 * t;
+    if(t < 1/2) return q;
+    if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    return p;
+}
---
+++ RGBConverter.h
@@ -0,0 +1,78 @@
+/*
+ * RGBConverter.h - Arduino library for converting between RGB, HSV and HSL
+ * 
+ * Ported from the Javascript at http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+ * The hard work was Michael's, all the bugs are mine.
+ *
+ * Robert Atkins, December 2010 (ratkins_at_fastmail_dot_fm).
+ *
+ * https://github.com/ratkins/RGBConverter
+ *
+ */  
+#ifndef RGBConverter_h
+#define RGBConverter_h
+
+#include "WProgram.h"
+
+class RGBConverter {
+
+public:
+    /**
+     * Converts an RGB color value to HSL. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes r, g, and b are contained in the set [0, 255] and
+     * returns h, s, and l in the set [0, 1].
+     *
+     * @param   byte    r       The red color value
+     * @param   byte    g       The green color value
+     * @param   byte    b       The blue color value
+     * @param   float  hsl[]   The HSL representation
+     */
+    void rgbToHsl(byte r, byte g, byte b, float hsl[]);
+    
+    /**
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   float  h       The hue
+     * @param   float  s       The saturation
+     * @param   float  l       The lightness
+     * @return  byte    rgb[]   The RGB representation
+     */
+    void hslToRgb(float h, float s, float l, byte rgb[]);
+
+    /**
+     * Converts an RGB color value to HSV. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+     * Assumes r, g, and b are contained in the set [0, 255] and
+     * returns h, s, and v in the set [0, 1].
+     *
+     * @param   byte  r       The red color value
+     * @param   byte  g       The green color value
+     * @param   byte  b       The blue color value
+     * @return  float hsv[]  The HSV representation
+     */
+    void rgbToHsv(byte r, byte g, byte b, float hsv[]);
+    
+    /**
+     * Converts an HSV color value to RGB. Conversion formula
+     * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+     * Assumes h, s, and v are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   float  h       The hue
+     * @param   float  s       The saturation
+     * @param   float  v       The value
+     * @return  byte    rgb[]   The RGB representation
+     */
+    void hsvToRgb(float h, float s, float v, byte rgb[]);
+     
+private:
+    byte threeway_max(byte a, byte b, byte c);
+    byte threeway_min(byte a, byte b, byte c);
+    float hue2rgb(float p, float q, float t);
+};
+
+#endif
